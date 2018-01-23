950dd1bdd676c1f07394b57355d609f6f76f1bb8
Xu Liangzhan <xlz26296x@163.com>
2018-01-24 00:02:19 +0800

    update 2.0.8

--- ajax/request.js
+++ ajax/request.js
@@ -53,7 +53,7 @@ Object.assign(XEAjaxRequest.prototype, {
       if (request.body && request.method !== 'GET') {
         try {
           if (isFunction(request.transformBody)) {
-            return Promise.resolve(request.transformBody(request.body, request))
+            result = request.transformBody(request.body, request)
           } else {
             if (isFormData(request.body)) {
               result = request.body
