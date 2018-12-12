1afce25dd486f1337918922ac617021a35b8bc32
Xu Liangzhan <xu_liangzhan@163.com>
2018-12-12 18:19:00 +0800

    update 3.5.0

--- dist/xe-ajax.js
+++ dist/xe-ajax.js
@@ -193,6 +193,31 @@
     }
   }
 
+  /**
+   * 进度条
+   *
+   * @param {Object} options 参数
+   */
+  function XEProgress (options) {
+    Object.assign(this, {
+      autoCompute: true,
+      fixed: 2,
+      meanSpeed: 0,
+      onDownloadProgress: null,
+      onUploadProgress: null
+    }, options, { _progress: { value: 0, total: 0, loaded: 0 } })
+  }
+
+  if (utils.IS_DP) {
+    utils.arrayEach('time,speed,loaded,value,total,remaining'.split(','), function (name) {
+      Object.defineProperty(XEProgress.prototype, name, {
+        get: function () {
+          return this._progress[name]
+        }
+      })
+    })
+  }
+
   function toHeaderKey (name) {
     return ('' + name).toLowerCase()
   }
