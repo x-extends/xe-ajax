ea23c9ea6a755a9c542463078d903760f460e96a
xuliangzhan <xu_liangzhan@163.com>
2019-01-11 18:35:36 +0800

    update 3.5.2

--- types/index.d.ts
+++ types/index.d.ts
@@ -45,14 +45,8 @@ export interface XEProgress<T> {
 
 export interface XERequestInterceptors {
   /**
-   * 使用
+   * 请求之前拦截器
    * @param resolve 请求发送之前执行
-   * @example 
-    ```javascript
-    XEAjax.interceptors.request.use((request, next) => {
-      next()
-    })
-    ```
    */
   use(
     resolve: (
@@ -64,34 +58,9 @@ export interface XERequestInterceptors {
 
 export interface XEResponseInterceptors {
   /**
-   * 使用
+   * 响应之后拦截器
    * @param onRejected 请求完成之后执行
    * @param onRejectd 发生错误之后执行
-   * @example 
-    ```javascript
-    XEAjax.interceptors.response.use((response, next) => {
-      next()
-    }, (e, next) => {
-      next()
-    })
-    // 改变响应结果
-    XEAjax.interceptors.response.use((response, next) => {
-      response.json().then(data => {
-        let { status, statusText, headers } = response
-        let body = {
-          message: status === 200 ? 'success' : 'error',
-          result: data
-        }
-        next({status, statusText, headers, body})
-      })
-    }, (e, next) => {
-      let body = {
-        message: 'error',
-        result: null
-      }
-      next({status: 200, body})
-    })
-    ```
    */
   use(
     onRejected: (
@@ -114,32 +83,6 @@ export interface XEAjaxMethods {
   /**
    * 设置全局参数
    * @param options 全局参数
-   * @example 
-    ```javascript
-    XEAjax.setup({
-      baseURL: 'https://xuliangzhan.com',
-      bodyType: 'json-data',
-      credentials: 'include',
-      headers: {
-        'Accept': 'application/json, text/plain, \*\/\*;'
-      },
-      validateStatus (response) {
-        return response.status >= 200 && response.status < 300
-      },
-      transformParams (params, request) {
-        return params
-      },
-      paramsSerializer (params, request) {
-        return XEAjax.serialize(params)
-      }，
-      transformBody (body, request) {
-        return body
-      },
-      stringifyBody (body, request) {
-        return JSON.stringify(body)
-      }
-    })
-    ```
    */
   setup(options: XEAjaxRequest<any>): void;
 
@@ -152,30 +95,12 @@ export interface XEAjaxMethods {
   /**
    * 将表单序列化为可以在 Ajax 请求中发送到服务器的查询字符串。
    * @param params 表单参数
-   * @example 
-    ```javascript
-    let param = XEAjax.serialize({id: 1, name: 'u2'})
-    // 'id=1&name=u2'
-    ```
    */
   serialize(params: object): string;
 
   /**
    * 请求之前、响应之后拦截器
    * @since 3.0.0
-   * @example 
-    ```javascript
-    // 请求之前
-    XEAjax.interceptors.request.use((request, next) => {
-      next()
-    })
-    // 响应之后
-    XEAjax.interceptors.response.use((response, next) => {
-      next()
-    }, (e, next) => {
-      next()
-    })
-    ```
    */
   interceptors: {
     /**
@@ -191,77 +116,23 @@ export interface XEAjaxMethods {
   /**
    * 上传、下载进度监听
    * @since 3.4.9
-   * @example 
-    ```javascript
-    let progress = new XEAjax.Progress()
-      progress.onUploadProgress = evnt => {
-        console.log(`进度：${progress.value}%`)
-      }
-      var file = document.querySelector('#myFile').files[0]
-      var formBody = new FormData()
-      formBody.append('file', file)
-      XEAjax.fetchPost('/api/upload', formBody, {progress})
-    ```
    */
   Progress: XEProgress<any>;
 
   /**
    * 取消控制器
    * @since 3.2.0
-   * @example 
-    ```javascript
-    let controller = new XEAjax.AbortController()
-    let signal = controller.signal
-    let body = {
-      name: 'u111',
-      content: '123456'
-    }
-    XEAjax.fetchPost('/api/test/message/save', body, {signal})
-    setTimeout(() => {
-      controller.abort()
-    }, 50)
-    ```
    */
   AbortController: AbortController;
 
   /**
    * @param options 请求参数
-   * @example 
-    ```javascript
-    XEAjax.ajax({
-      url: '/api/test/message/save',
-      method: 'POST',
-      body: {
-        id: 1,
-        name: 'u2'
-      }
-    }).then(response => {
-      response.json().then(data => {
-          console.log(result.data)
-        })
-      }).catch(e => {
-        console.log(e)
-      })
-    ```
    */
   ajax(options: XEAjaxRequest<any>): Promise<Response>;
 
   /**
    * doAll 使用对象参数, 用法和 Promise.all 一致
    * @param iterable 对象数组 或 Promise 数组
-   * @example 
-    ```javascript
-    XEAjax.doAll([
-      {
-        url: '/api/test/message/list',
-        method: 'GET'
-      }
-    ]).then(datas => {
-      // 所有异步完成之后执行
-    }).catch(e => {
-      console.log(e)
-    })
-    ```
    */
   doAll(iterable: (object | Promise<any>)[]): Promise<any[]>;
 
@@ -270,15 +141,6 @@ export interface XEAjaxMethods {
    * @param url 请求地址
    * @param params 表单查询参数
    * @param options 可选参数
-   * @example 
-    ```javascript
-    XEAjax.fetchJsonp('/jsonp/test/message/list')
-      .then(response => {
-        response.json().then(data => {
-          console.log(result.data)
-        })
-      })
-    ```
    */
   fetchJsonp(url: string, params?: any, options?: XEAjaxRequest<any>): Promise<Response>;
 
@@ -287,15 +149,6 @@ export interface XEAjaxMethods {
    * @param url 请求地址
    * @param params 表单查询参数
    * @param options 可选参数
-   * @example 
-    ```javascript
-    XEAjax.fetchGet('/api/test/message/list')
-      .then(response => {
-        response.json().then(data => {
-          console.log(result.data)
-        })
-      })
-    ```
    */
   fetchGet(url: string, params?: any, options?: XEAjaxRequest<any>): Promise<Response>;
 
@@ -304,28 +157,6 @@ export interface XEAjaxMethods {
    * @param url 请求地址
    * @param body 表单提交参数
    * @param options 可选参数
-   * @example 
-    ```javascript
-    // 使用 "application/json" 方式提交，默认使用 JSON.stringify 序列化数据
-    let body1 = {
-      name: 'u222',
-      content: '123456'
-    }
-    XEAjax.fetchPost('/api/test/message/save', body1)
-
-    // 使用 "application/x-www-form-urlencoded" 方式提交，默认使用 XEAjax.serialize 序列化数据
-    let body2 = {
-      name: 'u222',
-      content: '123456'
-    }
-    XEAjax.fetchPost('/api/test/message/save', body2, {bodyType: 'form-data'})
-
-    // 模拟表单 "multipart/form-data" 提交
-    let file = document.querySelector('#myFile').files[0]
-    let formBody = new FormData()
-    formBody.append('file', file)
-    XEAjax.fetchPost('/api/test/message/save', formBody)
-    ```
    */
   fetchPost(url: string, body?: any, options?: XEAjaxRequest<any>): Promise<Response>;
 
@@ -334,20 +165,6 @@ export interface XEAjaxMethods {
    * @param url 请求地址
    * @param body 表单提交参数
    * @param options 可选参数
-   * @example 
-    ```javascript
-    let body = {
-      id: 1,
-      name: 'u222',
-      content: '123456'
-    }
-    XEAjax.fetchPut('/api/test/message/update', body)
-      .then(response => {
-        response.json().then(data => {
-          console.log(result.data)
-        })
-      })
-    ```
    */
   fetchPut(url: string, body?: any, options?: XEAjaxRequest<any>): Promise<Response>;
 
@@ -355,15 +172,6 @@ export interface XEAjaxMethods {
    * 发送 DELETE 请求，返回一个结果为 Response 的 Promise 对象
    * @param url 请求地址
    * @param options 可选参数
-   * @example 
-    ```javascript
-    XEAjax.fetchDelete('/api/test/message/delete/1')
-      .then(response => {
-        response.json().then(data => {
-          console.log(result.data)
-        })
-      })
-    ```
    */
   fetchDelete(url: string, options?: XEAjaxRequest<any>): Promise<Response>;
 
@@ -385,15 +193,6 @@ export interface XEAjaxMethods {
    * 发送请求，返回一个结果为 Response 的 Promise 对象
    * @param url 请求地址
    * @param options 可选参数
-   * @example 
-    ```javascript
-    XEAjax.fetch('/api/test/message/list')
-      .then(response => {
-        response.json().then(data => {
-          console.log(result.data)
-        })
-      })
-    ```
    */
   fetch(url: string, options?: XEAjaxRequest<any>): Promise<Response>;
 
@@ -403,13 +202,6 @@ export interface XEAjaxMethods {
    * @param params 表单查询参数
    * @param options 可选参数
    * @since 3.4.0
-   * @example 
-    ```javascript
-    XEAjax.doJsonp('/jsonp/test/message/list')
-      .then(result => {
-        console.log(result.data)
-      })
-    ```
    */
   doJsonp(url: string, params?: any, options?: XEAjaxRequest<any>): Promise<XEAjaxResponseSchema<any>>;
 
@@ -419,15 +211,6 @@ export interface XEAjaxMethods {
    * @param params 表单查询参数
    * @param options 可选参数
    * @since 3.4.0
-   * @example 
-    ```javascript
-    XEAjax.doGet('/api/test/message/list')
-      .then(result => {
-        console.log(result.data)
-      }).catch(e => {
-        
-      })
-    ```
    */
   doGet(url: string, params?: any, options?: XEAjaxRequest<any>): Promise<XEAjaxResponseSchema<any>>;
 
@@ -437,28 +220,6 @@ export interface XEAjaxMethods {
    * @param body 表单提交参数
    * @param options 可选参数
    * @since 3.4.0
-   * @example 
-    ```javascript
-    // 使用 "application/json" 方式提交，默认使用 JSON.stringify 序列化数据
-    let body1 = {
-      name: 'u222',
-      content: '123456'
-    }
-    XEAjax.doPost('/api/test/message/save', body1)
-
-    // 使用 "application/x-www-form-urlencoded" 方式提交，默认使用 XEAjax.serialize 序列化数据
-    let body2 = {
-      name: 'u222',
-      content: '123456'
-    }
-    XEAjax.doPost('/api/test/message/save', body2, {bodyType: 'form-data'})
-
-    // 模拟表单 "multipart/form-data" 提交
-    let file = document.querySelector('#myFile').files[0]
-    let formBody = new FormData()
-    formBody.append('file', file)
-    XEAjax.doPost('/api/test/message/save', formBody)
-    ```
    */
   doPost(url: string, body?: any, options?: XEAjaxRequest<any>): Promise<XEAjaxResponseSchema<any>>;
 
@@ -468,20 +229,6 @@ export interface XEAjaxMethods {
    * @param body 表单提交参数
    * @param options 可选参数
    * @since 3.4.0
-   * @example 
-    ```javascript
-    let body = {
-      id: 1,
-      name: 'u222',
-      content: '123456'
-    }
-    XEAjax.doPut('/api/test/message/update', body)
-      .then(result => {
-        console.log(result.data)
-      }).catch(e => {
-
-      })
-    ```
    */
   doPut(url: string, body?: any, options?: XEAjaxRequest<any>): Promise<XEAjaxResponseSchema<any>>;
 
@@ -490,15 +237,6 @@ export interface XEAjaxMethods {
    * @param url 请求地址
    * @param options 可选参数
    * @since 3.4.0
-   * @example 
-    ```javascript
-    XEAjax.doDelete('/api/test/message/delete/1')
-      .then(result => {
-        console.log(result.data)
-      }).catch(e => {
-        
-      })
-    ```
    */
   doDelete(url: string, options?: XEAjaxRequest<any>): Promise<XEAjaxResponseSchema<any>>;
 
@@ -523,15 +261,6 @@ export interface XEAjaxMethods {
    * @param url 请求地址
    * @param params 表单查询参数
    * @param options 可选参数
-   * @example 
-    ```javascript
-    XEAjax.jsonp('/jsonp/test/message/list')
-      .then(data => {
-        console.log(data)
-      }).catch(e => {
-
-      })
-    ```
    */
   jsonp(url: string, params?: any, options?: XEAjaxRequest<any>): Promise<any>;
 
@@ -540,15 +269,6 @@ export interface XEAjaxMethods {
    * @param url 请求地址
    * @param params 表单查询参数
    * @param options 可选参数
-   * @example 
-    ```javascript
-    XEAjax.getJSON('/api/test/message/list')
-      .then(data => {
-        console.log(data)
-      }).catch(e => {
-        
-      })
-    ```
    */
   getJSON(url: string, params?: any, options?: XEAjaxRequest<any>): Promise<any>;
 
@@ -557,28 +277,6 @@ export interface XEAjaxMethods {
    * @param url 请求地址
    * @param body 表单提交参数
    * @param options 可选参数
-   * @example 
-    ```javascript
-    // 使用 "application/json" 方式提交，默认使用 JSON.stringify 序列化数据
-    let body1 = {
-      name: 'u222',
-      content: '123456'
-    }
-    XEAjax.postJSON('/api/test/message/save', body1)
-
-    // 使用 "application/x-www-form-urlencoded" 方式提交，默认使用 XEAjax.serialize 序列化数据
-    let body2 = {
-      name: 'u222',
-      content: '123456'
-    }
-    XEAjax.postJSON('/api/test/message/save', body2, {bodyType: 'form-data'})
-
-    // 模拟表单 "multipart/form-data" 提交
-    let file = document.querySelector('#myFile').files[0]
-    let formBody = new FormData()
-    formBody.append('file', file)
-    XEAjax.postJSON('/api/test/message/save', formBody)
-    ```
    */
   postJSON(url: string, body?: any, options?: XEAjaxRequest<any>): Promise<any>;
 
@@ -587,20 +285,6 @@ export interface XEAjaxMethods {
    * @param url 请求地址
    * @param body 表单提交参数
    * @param options 可选参数
-   * @example 
-    ```javascript
-    let body = {
-      id: 1,
-      name: 'u222',
-      content: '123456'
-    }
-    XEAjax.putJSON('/api/test/message/update', body)
-      .then(data => {
-        console.log(data)
-      }).catch(e => {
-        
-      })
-    ```
    */
   putJSON(url: string, body?: any, options?: XEAjaxRequest<any>): Promise<any>;
 
@@ -608,15 +292,6 @@ export interface XEAjaxMethods {
    * 发送 DELETE 请求，返回响应结果为 JSON 的 Peomise 对象
    * @param url 请求地址
    * @param options 可选参数
-   * @example 
-    ```javascript
-    XEAjax.deleteJSON('/api/test/message/delete/1')
-      .then(data => {
-        console.log(data)
-      }).catch(e => {
-        
-      })
-    ```
    */
   deleteJSON(url: string, options?: XEAjaxRequest<any>): Promise<any>;
 
